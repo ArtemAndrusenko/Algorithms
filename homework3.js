f6b73781c6825f2b47201f9e01ccf5d321aa7844
Andrusenko.Artem <andrusenkoartem2005@gmail.com>
2023-10-22 20:16:52 +0200

    first commit

---
+++ homework3.js
@@ -0,0 +1,17 @@
+//Итеративный подход
+-
+
+//Рекурсивный подход
+
+function recursive(n, left, middle, right) {
+    if (n === 1) {
+        console.log(`Move disk from ${left} to ${right}`);
+        return;
+    } //если кольцо 1, то из левого перекинуть на правое
+
+    recursive(n - 1, left, right, middle); // перемещаем n-1 дисков с левого стержня на средний, используя правый как вспомогательный
+    console.log(`Move disk from ${left} to ${right}`); // перемещаем самый большой диск с левого стержня на правый
+    recursive(n - 1, middle, left, right); // перемещаем n-1 дисков со среднего стержня на правый стержень, используя левый как вспомогательный
+}
+
+recursive(3, 'Left', 'Middle', 'Right');
